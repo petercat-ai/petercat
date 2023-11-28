056863b5bd109582117fc8ad736457bd51fbbfb1
yingying <yingying.xwy@antgroup.com>
2023-11-28 17:01:13 +0800

    chore: init the repo

--- app/retrieval/page.tsx
+++ app/retrieval/page.tsx
@@ -4,7 +4,7 @@ export default function AgentsPage() {
   const InfoCard = (
     <div className="p-4 md:p-8 rounded bg-[#25252d] w-full max-h-[85%] overflow-hidden">
       <h1 className="text-3xl md:text-4xl mb-4">
-        ▲ Next.js + LangChain.js Retrieval Chain 🦜🔗
+        🦉 xuexiao
       </h1>
       <ul>
         <li className="hidden text-l md:block">
