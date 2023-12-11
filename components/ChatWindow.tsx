7ab7145ca5a55327db1975674bfd5eaf8b4c83ba
raohai.rh <raohai.rh@antgroup.com>
2023-12-11 16:51:23 +0800

    feat: support streamming conversation and abort

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -2,15 +2,14 @@
 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-import { useChat } from 'ai/react';
-import { useRef, useState, ReactElement, useEffect } from 'react';
+import { useRef, useState, ReactElement, useCallback } from 'react';
 import type { FormEvent } from 'react';
-import type { AgentStep } from 'langchain/schema';
+import { Button } from '@nextui-org/react';
 
 import { ChatMessageBubble } from '@/components/ChatMessageBubble';
 import { UploadDocumentsForm } from '@/components/UploadDocumentsForm';
 import { IntermediateStep } from './IntermediateStep';
+import { useChat } from './hooks/useChat';
 
 export function ChatWindow(props: {
   endpoint: string;
@@ -21,6 +20,7 @@ export function ChatWindow(props: {
   showIngestForm?: boolean;
   showIntermediateStepsToggle?: boolean;
   prompt?: string;
+  streamming?: boolean;
 }) {
   const messageContainerRef = useRef<HTMLDivElement | null>(null);
 
@@ -61,24 +61,12 @@ export function ChatWindow(props: {
   const {
     messages,
     input,
-    setInput,
     handleInputChange,
     handleSubmit,
     isLoading: chatEndpointIsLoading,
-    setMessages,
+    stop,
   } = useChat({
     api: endpoint,
-    onResponse(response) {
-      const sourcesHeader = response.headers.get('x-sources');
-      const sources = sourcesHeader ? JSON.parse(atob(sourcesHeader)) : [];
-      const messageIndexHeader = response.headers.get('x-message-index');
-      if (sources.length && messageIndexHeader !== null) {
-        setSourcesForMessages({
-          ...sourcesForMessages,
-          [messageIndexHeader]: sources,
-        });
-      }
-    },
     onError: (e) => {
       toast(e.message, {
         theme: 'light',
@@ -86,79 +74,16 @@ export function ChatWindow(props: {
     },
   });
 
-  async function sendMessage(e: FormEvent<HTMLFormElement>) {
+  const sendMessage = useCallback((e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (messageContainerRef.current) {
-      messageContainerRef.current.classList.add('grow');
-    }
-    if (!messages.length) {
-      await new Promise((resolve) => setTimeout(resolve, 300));
-    }
-    if (chatEndpointIsLoading ?? intermediateStepsLoading) {
-      return;
-    }
-    if (!showIntermediateSteps) {
-      handleSubmit(e);
-      // Some extra work to show intermediate steps properly
-    } else {
-      setIntermediateStepsLoading(true);
-      setInput('');
-      const messagesWithUserReply = messages.concat({
-        id: messages.length.toString(),
-        content: input,
-        role: 'user',
+    if (!chatEndpointIsLoading) {
+      return handleSubmit({
+        prompt,
+        show_intermediate_steps: true,
       });
-      setMessages(messagesWithUserReply);
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        body: JSON.stringify({
-          messages: messagesWithUserReply,
-          prompt: prompt,
-          show_intermediate_steps: true,
-        }),
-      });
-
-      const json = await response.json();
-      setIntermediateStepsLoading(false);
-      if (response.status === 200) {
-        // Represent intermediate steps as system messages for display purposes
-        const intermediateStepMessages = (json.intermediate_steps ?? []).map(
-          (intermediateStep: AgentStep, i: number) => {
-            return {
-              id: (messagesWithUserReply.length + i).toString(),
-              content: JSON.stringify(intermediateStep),
-              role: 'system',
-            };
-          },
-        );
-        const newMessages = messagesWithUserReply;
-        for (const message of intermediateStepMessages) {
-          newMessages.push(message);
-          setMessages([...newMessages]);
-          await new Promise((resolve) =>
-            setTimeout(resolve, 1000 + Math.random() * 1000),
-          );
-        }
-        setMessages([
-          ...newMessages,
-          {
-            id: (
-              newMessages.length + intermediateStepMessages.length
-            ).toString(),
-            content: json.output,
-            role: 'assistant',
-          },
-        ]);
-      } else {
-        if (json.error) {
-          toast(json.error, {
-            theme: 'light',
-          });
-          throw new Error(json.error);
-        }
-      }
     }
-  }
+    stop();
+  }, [chatEndpointIsLoading, handleSubmit, prompt, stop]);
 
   return (
     <div
@@ -171,7 +96,7 @@ export function ChatWindow(props: {
           messages.length > 0 ? '' : 'hidden'
         } text-2xl flex items-center`}
       >
-        <img className="w-12 rounded-full  m-2" src={avatar} />
+        <img className="w-12 rounded-full  m-2" alt={titleText} src={avatar!} />
         <div className="flex">{titleText}</div>
       </h2>
       {messages.length === 0 ? emptyStateComponent : ''}
@@ -207,46 +132,9 @@ export function ChatWindow(props: {
             placeholder={placeholder ?? "What's it like to be a pirate?"}
             onChange={handleInputChange}
           />
-          <button
-            type="submit"
-            className="shrink-0 px-8 py-4 bg-sky-600 rounded w-28"
-          >
-            <div
-              role="status"
-              className={`${
-                chatEndpointIsLoading || intermediateStepsLoading
-                  ? ''
-                  : 'hidden'
-              } flex justify-center`}
-            >
-              <svg
-                aria-hidden="true"
-                className="w-6 h-6 text-white animate-spin dark:text-white fill-sky-800"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-              <span className="sr-only">Loading...</span>
-            </div>
-            <span
-              className={
-                chatEndpointIsLoading || intermediateStepsLoading
-                  ? 'hidden'
-                  : ''
-              }
-            >
-              Send
-            </span>
-          </button>
+          <Button type="submit" radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+            {chatEndpointIsLoading ? 'Cancel' : 'Send'}
+          </Button>
         </div>
       </form>
       <ToastContainer />
---
+++ components/hooks/useChat.ts
@@ -0,0 +1,185 @@
+
+import { uniqueId } from 'lodash';
+import { useCallback, useEffect, useId, useRef, useState } from 'react';
+import useSWR from 'swr';
+
+export interface Message {
+  id: string;
+  createdAt?: Date;
+  content: string;
+  ui?: string | JSX.Element | JSX.Element[] | null | undefined;
+  role: 'system' | 'user' | 'assistant' | 'function' | 'data';
+}
+
+export interface UseChatOptions {
+  api: string;
+  initialMessages?: Message[];
+  initialInput?: string;
+  onError?: (error: Error) => void;
+}
+
+export interface ChatRequest {
+  messages: Message[];
+  options: ChatRequestOptions;
+}
+
+export interface ChatRequestOptions {}
+
+export function useChat({
+  api,
+  initialMessages = [],
+  initialInput = '',
+  onError,
+}: UseChatOptions) {
+  const hookId = useId();
+  const chatKey = typeof api === 'string' ? [api, hookId] : hookId;
+
+  const { data: messages = [], mutate } = useSWR<Message[]>([chatKey, 'messages'], null, { fallbackData: initialMessages });
+  const { data: isLoading = false, mutate: mutateLoading } = useSWR<boolean>([chatKey, 'loading'], null);
+  const { data: streamData, mutate: mutateStreamData } = useSWR<{}[] | undefined>([chatKey, 'streamData'], null);
+
+  const setMessages = useCallback(
+    (messages: Message[]) => {
+      mutate(messages, false);
+      messagesRef.current = messages;
+    },
+    [mutate],
+  );
+
+  // Keep the latest messages in a ref.
+  const messagesRef = useRef<Message[]>(messages || []);
+  useEffect(() => {
+    messagesRef.current = messages || [];
+  }, [messages]);
+
+  // Input state and handlers.
+  const [input, setInput] = useState(initialInput);
+
+  // Abort controller to cancel the current API call.
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  const triggerRequest = useCallback(async (chatRequest: ChatRequest) => {
+    const abortController = new AbortController();
+    abortControllerRef.current = abortController;
+
+    try {
+      mutateLoading(true);
+      // positive update messages
+      mutate(chatRequest.messages, false);
+
+      const promise = fetch(api, {
+        method: 'POST',
+        body: JSON.stringify({
+          messages: chatRequest.messages,
+          ...chatRequest.options,
+        }),
+        signal: abortControllerRef.current?.signal,
+      });
+
+      await promise.then(response => {
+        mutate([
+          ...chatRequest.messages,
+          {
+            id: uniqueId(),
+            content: '',
+            role: 'assistant',
+          },
+        ], false);
+
+        const reader = response.body!.getReader();
+        return new ReadableStream({
+          start(controller) {
+            function read(): Promise<string | undefined> {
+              return reader.read().then(({ done, value }) => {
+                mutate((prevMessages = []) => {
+                  const lastMessage = prevMessages.at(-1)!;
+                  const text = new TextDecoder().decode(value);
+                  return [
+                    ...prevMessages.slice(0, -1),
+                    {
+                      ...lastMessage,
+                      content: lastMessage.content.concat(text),
+                    },
+                  ];
+                }, false);
+
+                if (done) {
+                  controller.close();
+                  return;
+                }
+
+                controller.enqueue(value);
+                return read();
+              });
+            }
+
+            return read();
+          },
+        });
+      })
+      .then((stream) => new Response(stream))
+      .then((response) => response.blob());
+    } catch (err) {
+      if ((err as any).name === 'AbortError') {
+        abortControllerRef.current = null;
+        return null;
+      }
+
+      if (onError && err instanceof Error) {
+        onError(err);
+      }
+    } finally {
+      mutateLoading(false);
+    }
+  }, [api, mutate, mutateLoading, onError, abortControllerRef]);
+
+  const append = useCallback(async (message: Message, options: ChatRequestOptions) => {
+    if (!message.id) {
+      message.id = uniqueId();
+    }
+
+    const chatRequest = {
+      messages: messagesRef.current.concat(message),
+      options,
+    };
+
+    return await triggerRequest(chatRequest);
+  }, [triggerRequest]);
+
+  const handleSubmit = useCallback((options: ChatRequestOptions) => {
+    if (!input) return;
+    append({
+      content: input,
+      role: 'user',
+      createdAt: new Date(),
+    } as Message, options);
+
+    setInput('');
+  },
+  [append, input]);
+
+  const handleInputChange = (e: any) => {
+    setInput(e.target.value);
+  };
+
+  const stop = useCallback(() => {
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+      abortControllerRef.current = null;
+    }
+  }, []);
+
+  return {
+    messages,
+    setMessages,
+
+    input,
+    setInput,
+
+    handleSubmit,
+    isLoading,
+
+    handleInputChange,
+    stop,
+  };
+}
