28d7c453c10f2441f1c44426e1ca64c2f73ef225
yingying <yingying.xwy@antgroup.com>
2023-12-14 11:53:19 +0800

    chore: test the python server

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,9 +4,7 @@ import React, { useState } from 'react';
 export default function Test() {
   const [message, setMessage] = useState();
 
-  const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
-
-  fetch(`${url}/api/test`)
+  fetch(`/api/test`)
     .then((res) => res.json())
     .then((data) => {
       setMessage(data.message);
