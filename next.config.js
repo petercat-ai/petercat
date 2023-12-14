2f2b0ff669ba09f36e58316a933c69f94c3e7913
yingying <yingying.xwy@antgroup.com>
2023-12-14 12:10:23 +0800

    chore: fix ci

--- next.config.js
+++ next.config.js
@@ -13,7 +13,10 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
       },
     ];
   },
