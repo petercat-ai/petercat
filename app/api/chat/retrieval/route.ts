9ca5ed16237209ff05108c79bc6718727e242fe0
yingying <yingying.xwy@antgroup.com>
2023-12-21 17:22:25 +0800

    chore: fix input

--- app/api/chat/retrieval/route.ts
+++ app/api/chat/retrieval/route.ts
@@ -1,40 +1,40 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
+import { NextRequest, NextResponse } from 'next/server';
+import { Message as VercelChatMessage, StreamingTextResponse } from 'ai';
 
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { PromptTemplate } from "langchain/prompts";
-import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
-import { Document } from "langchain/document";
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { PromptTemplate } from 'langchain/prompts';
+import { SupabaseVectorStore } from 'langchain/vectorstores/supabase';
+import { Document } from 'langchain/document';
 import {
   RunnableSequence,
   RunnablePassthrough,
-} from "langchain/schema/runnable";
+} from 'langchain/schema/runnable';
 import {
   BytesOutputParser,
   StringOutputParser,
-} from "langchain/schema/output_parser";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+} from 'langchain/schema/output_parser';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
-const combineDocumentsFn = (docs: Document[], separator = "\n\n") => {
+const combineDocumentsFn = (docs: Document[], separator = '\n\n') => {
   const serializedDocs = docs.map((doc) => doc.pageContent);
   return serializedDocs.join(separator);
 };
 
 const formatVercelMessages = (chatHistory: VercelChatMessage[]) => {
   const formattedDialogueTurns = chatHistory.map((message) => {
-    if (message.role === "user") {
+    if (message.role === 'user') {
       return `Human: ${message.content}`;
-    } else if (message.role === "assistant") {
+    } else if (message.role === 'assistant') {
       return `Assistant: ${message.content}`;
     } else {
       return `${message.role}: ${message.content}`;
     }
   });
-  return formattedDialogueTurns.join("\n");
+  return formattedDialogueTurns.join('\n');
 };
 
 const CONDENSE_QUESTION_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
@@ -79,18 +79,18 @@ export async function POST(req: NextRequest) {
     const currentMessageContent = messages[messages.length - 1].content;
 
     const model = new ChatOpenAI({
-      modelName: "gpt-3.5-turbo",
-      temperature: 0.2
+      modelName: 'gpt-3.5-turbo',
+      temperature: 0.2,
     });
 
     const client = createClient(
       process.env.SUPABASE_URL!,
-      process.env.SUPABASE_PRIVATE_KEY!,
+      process.env.SUPABASE_API_KEY!,
     );
     const vectorstore = new SupabaseVectorStore(new OpenAIEmbeddings(), {
       client,
-      tableName: "documents",
-      queryName: "match_documents",
+      tableName: 'documents',
+      queryName: 'match_documents',
     });
 
     /**
@@ -154,17 +154,17 @@ export async function POST(req: NextRequest) {
       JSON.stringify(
         documents.map((doc) => {
           return {
-            pageContent: doc.pageContent.slice(0, 50) + "...",
+            pageContent: doc.pageContent.slice(0, 50) + '...',
             metadata: doc.metadata,
           };
         }),
       ),
-    ).toString("base64");
+    ).toString('base64');
 
     return new StreamingTextResponse(stream, {
       headers: {
-        "x-message-index": (previousMessages.length + 1).toString(),
-        "x-sources": serializedSources,
+        'x-message-index': (previousMessages.length + 1).toString(),
+        'x-sources': serializedSources,
       },
     });
   } catch (e: any) {
