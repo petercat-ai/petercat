1556c427475aeb14ff8fbb18ff3c4bb9665ba382
yingying <yingying.xwy@antgroup.com>
2024-05-07 17:28:53 +0800

    feat: add the knowledge tool

--- server/agent/stream.py
+++ server/agent/stream.py
@@ -1,26 +1,21 @@
-import datetime
 import json
-import os
 import uuid
-from langchain.tools import tool
 from typing import AsyncIterator
 from langchain.agents import AgentExecutor
 from data_class import ChatData, Message
 from langchain.agents.format_scratchpad.openai_tools import (
     format_to_openai_tool_messages,
 )
+from langchain_core.messages import AIMessage, FunctionMessage, HumanMessage
 from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
-from langchain.prompts import MessagesPlaceholder
+from langchain.prompts import PromptTemplate, MessagesPlaceholder
 from langchain_core.utils.function_calling import convert_to_openai_tool
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.utilities.tavily_search import TavilySearchAPIWrapper
 from langchain.tools.tavily_search import TavilySearchResults
 from langchain_openai import ChatOpenAI
 from uilts.env import get_env_variable
-from tools import issue
-from tools import sourcecode
-from langchain_core.messages import AIMessage, FunctionMessage, HumanMessage
-
+from tools import issue, sourcecode, knowledge
 TAVILY_API_KEY =  get_env_variable("TAVILY_API_KEY")
 
 prompt = ChatPromptTemplate.from_messages(
@@ -28,13 +23,18 @@ prompt = ChatPromptTemplate.from_messages(
         (
             "system",
             "Character: Ant Design Bot"
-            "You are a question-and-answer robot specifically dedicated to providing solutions for Ant Design. You are well-versed in Ant Design-related knowledge and capable of assisting in fixing code issues related to it. However, you are unable to answer questions not related to Ant Design."
+            "You are a question-and-answer robot specifically dedicated to providing solutions and information  for Ant Design. You are well-versed in Ant Design-related knowledge and capable of assisting in fixing code issues related to it."
             "Skill 1: Normal Conversation"
             "In most cases, you will have ordinary conversations with users, answering questions about Ant Design."
-            "Skill 2: Issue Handling"
+            "Skill 2: Knowledge Search"
+            "When users ask about issues or something you don't know, you will choose to use the appropriate search tools for handling based on the description of actions. There are two search tools you can use:"
+            "1. search_knowledge: You can use it first to search knowledge that you do not sure about Ant Design. When use this tool, do not translate the search query. Use the original query language to search. eg: When user's question is 'Ant Design 有哪些新特性?', the query should be 'Ant Design 有哪些新特性?'."
+            "2. tavily_search_results_json: You can use this tool to search information that you can't find by using search_knowledge."
+            "Skill 3: Issue Handling"
             "When users ask about issues, you will choose to use the appropriate tools for handling based on the description of actions. There could be two scenarios:"
             "1. Talk with the user as normal. "
-            "2. If they ask you about issues, use a tool"
+            "2. If they ask you about issues, use a tool like create_issue, get_issues, search_issues, search_code, etc. to help them."
+            "3. If they ask you about issues, use tools like create_issue, get_issues, search_issues, search_code, etc. to help them."
             "Constraints:"
             "Only answer questions related to Ant Design; if users pose unrelated questions, you need to inform the user that you cannot answer and guide them to ask questions related to Ant Design."
             "Decide whether to use relevant tools for handling based on the nature of the question and the description of actions."
@@ -46,26 +46,28 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-@tool
-def get_datetime() -> datetime:
-    """Get current date time."""
-    return datetime.now().strftime("%H:%M:%S")
-
 TOOL_MAPPING = {
-    "get_datetime": get_datetime,
+    "search_knowledge": knowledge.search_knowledge,
     "create_issue": issue.create_issue,
     "get_issues": issue.get_issues,
     "search_issues": issue.search_issues,
     "search_code": sourcecode.search_code,
 }
-TOOLS = ["get_datetime", "create_issue", "get_issues", "search_issues",  "search_code"]
+TOOLS = ["search_knowledge", "create_issue", "get_issues", "search_issues",  "search_code"]
 
+def init_default_tools():
+    # init Tavily 
+    search = TavilySearchAPIWrapper()
+    tavily_tool = TavilySearchResults(api_wrapper=search)
+    
+    return [tavily_tool]
+    
 
 def _create_agent_with_tools(open_api_key: str) -> AgentExecutor:
     llm = ChatOpenAI(model="gpt-4-1106-preview", temperature=0.2, streaming=True, max_tokens=1500, openai_api_key=open_api_key)
-    search = TavilySearchAPIWrapper()
-    tavily_tool = TavilySearchResults(api_wrapper=search)
-    tools = [tavily_tool]
+   
+   
+    tools = init_default_tools();
 
     for requested_tool in TOOLS:
         if requested_tool not in TOOL_MAPPING:
@@ -91,9 +93,9 @@ def _create_agent_with_tools(open_api_key: str) -> AgentExecutor:
         | llm_with_tools
         | OpenAIToolsAgentOutputParser()
     )
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True).with_config(
-        {"run_name": "agent"}
-    )
+  
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True,
+max_iterations = 5)
     return agent_executor
 
 
@@ -115,6 +117,7 @@ async def agent_chat(input_data: ChatData, open_api_key: str) -> AsyncIterator[s
         messages = input_data.messages
         agent_executor = _create_agent_with_tools(open_api_key)
         print(chat_history_transform(messages))
+       
         async for event in agent_executor.astream_events(
             {
                 "input": messages[len(messages) - 1].content,
---
+++ server/tools/knowledge.py
@@ -0,0 +1,19 @@
+from langchain.tools import tool
+from rag import retrieval
+
+
+@tool
+def search_knowledge(
+      query: str,
+):
+    """
+    Search for information based on the query.  When use this tool, do not translate the search query. Use the original query language to search. eg: When user's question is 'Ant Design 有哪些新特性?', the query should be 'Ant Design 有哪些新特性?'.
+
+    :param query: The user's question.
+    """
+    try:
+        return retrieval.search_knowledge(query)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+  
