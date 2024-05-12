f2635f7b202f755812361e80ff5afc2b43d1cf01
陆沉 <surgesoft@gmail.com>
2024-05-12 21:57:35 +0800

    feat: impl rate limit and throw 429 if too many requests

---
+++ server/verify/rate_limit.py
@@ -0,0 +1,45 @@
+
+from fastapi import Cookie, HTTPException
+from datetime import datetime, timedelta
+
+from auth.get_user_info import getUserInfoByToken
+from db.supabase.client import get_client
+
+RATE_LIMIT_REQUESTS = 100
+RATE_LIMIT_DURATION = timedelta(minutes=1)
+
+async def verify_rate_limit(petercat: str = Cookie(None)):
+    if not petercat:
+        raise HTTPException(status_code=403, detail="Must Login")
+    user = await getUserInfoByToken(petercat)
+    user_id = user['id']
+
+    supabase = get_client()
+    table = supabase.table("user_token_usage")
+    rows = table.select('id, user_id, last_request, request_count').eq('user_id', user_id).execute()
+
+    now = datetime.now().isoformat()
+    user_usage = rows.data[0] if len(rows.data) > 0 else { "user_id": user_id, 'request_count': 0, 'last_request': now }
+    print('user_usage', user_usage)
+    # Calculate the time elapsed since the last request
+    elapsed_time = datetime.now() - datetime.fromisoformat(user_usage["last_request"])
+    
+    if elapsed_time > RATE_LIMIT_DURATION:
+        # If the elapsed time is greater than the rate limit duration, reset the count
+        user_usage['request_count'] = 1
+    else:
+        if user_usage['request_count'] >= RATE_LIMIT_REQUESTS:
+            # If the request count exceeds the rate limit, return a JSON response with an error message
+            raise HTTPException(
+                status_code=429, 
+                detail="Rate Limit Exceeded, Try It Later",
+                headers={"Retry-After": "60"}
+            )
+
+        user_usage['request_count'] = int(user_usage['request_count']) + 1
+    
+    user_usage['last_request'] = datetime.now().isoformat()
+
+    table.upsert(user_usage).execute()
+
+    return user
