0cf4fb38104d1e478ced6d6791d4d920d78deb70
yumou.zhangym <yumou.zhangym@antgroup.com>
2024-05-30 10:14:27 +0800

    feat: fix dependencies and button href

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -97,7 +97,7 @@ export function Navbar() {
           {!pathname.includes('/factory/list') && (
             <Button className='bg-[#3F3F46] text-[#FFFFFF] rounded-full px-4 py-2' startContent={<StoreIcon/>}>上架机器人</Button>
           )}
-          {pathname.includes('/factory/list') && (
+          {pathname.includes('/factory/edit/new') && (
             <Button className='bg-[#3F3F46] text-[#FFFFFF] rounded-full px-4 py-2' startContent={<AddIcon/>}>创建机器人</Button>
           )}
           
