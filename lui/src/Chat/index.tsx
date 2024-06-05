d48a587ccbe242a963bb083957757163fc8482f9
yingying <yingying.xwy@antgroup.com>
2024-06-05 15:35:58 +0800

    ref: enanle user to inject custom prompt by the LUI component

--- lui/src/Chat/index.tsx
+++ lui/src/Chat/index.tsx
@@ -16,8 +16,8 @@ import { streamChat } from '../services/ChatController';
 import { handleStream } from '../utils';
 import InputArea from './inputArea/InputArea';
 
-import Actions from './inputArea/actions';
 import '../style/global.css';
+import Actions from './inputArea/actions';
 
 const { getDesignToken } = theme;
 const globalToken = getDesignToken();
@@ -27,161 +27,188 @@ export interface ChatProps {
   helloMessage?: string;
   apiUrl?: string;
   drawerWidth?: number;
-  suggested_questions?: string[];
+  starters?: string[];
+  prompt?: string;
+  token?: string;
+  style?: React.CSSProperties;
 }
 
-const Chat: FC<ChatProps> = memo(({ helloMessage, apiUrl, drawerWidth, assistantMeta, suggested_questions }) => {
-  const proChatRef = useRef<ProChatInstance>();
-  const [chats, setChats] = useState<ChatMessage<Record<string, any>>[]>();
-  const messageMinWidth = drawerWidth
-    ? `calc(${drawerWidth}px - 90px)`
-    : '100%';
-  return (
-    <div className="petercat-lui" style={{ height: '100%' }}>
+const Chat: FC<ChatProps> = memo(
+  ({
+    helloMessage,
+    apiUrl,
+    drawerWidth,
+    assistantMeta,
+    starters,
+    prompt,
+    token,
+    style,
+  }) => {
+    const proChatRef = useRef<ProChatInstance>();
+    const [chats, setChats] = useState<ChatMessage<Record<string, any>>[]>();
+    const messageMinWidth = drawerWidth
+      ? `calc(${drawerWidth}px - 90px)`
+      : '100%';
+    return (
       <div
-        className="h-full w-full"
-        style={{ backgroundColor: globalToken.chatBoxBackgroundColor }}
+        className={`petercat-lui bg-[${globalToken.chatBoxBackgroundColor}]`}
+        style={{
+          ...style,
+          height: '100%',
+        }}
       >
-        <ProChat
-          showTitle
-          chats={chats}
-          onChatsChange={(chats) => {
-            setChats(chats);
-          }}
-          chatRef={proChatRef}
-          helloMessage={helloMessage || BOT_INFO.work_info.prologue}
-          userMeta={{ title: 'User' }}
-          chatItemRenderConfig={{
-            avatarRender: (props: ChatItemProps) => {
-              if (props.originData?.role === Role.user) {
-                return <></>;
-              }
-              if (
-                props.originData?.role === Role.tool ||
-                props.originData?.role === Role.knowledge
-              ) {
-                return <div className="w-[40px] h-[40px]" />;
-              }
-            },
-            contentRender: (props: ChatItemProps, defaultDom: ReactNode) => {
-              const originData = props.originData || {};
-              if (originData?.role === Role.user) {
-                return defaultDom;
-              }
-              const message = originData.content;
-              const defaultMessageContent = (
-                <div className="leftMessageContent" style={{minWidth: messageMinWidth }}>{defaultDom}</div>
-              );
+        <div className="h-full w-full">
+          <ProChat
+            showTitle
+            chats={chats}
+            onChatsChange={(chats) => {
+              setChats(chats);
+            }}
+            chatRef={proChatRef}
+            helloMessage={helloMessage || BOT_INFO.work_info.prologue}
+            userMeta={{ title: 'User' }}
+            chatItemRenderConfig={{
+              avatarRender: (props: ChatItemProps) => {
+                if (props.originData?.role === Role.user) {
+                  return <></>;
+                }
+                if (
+                  props.originData?.role === Role.tool ||
+                  props.originData?.role === Role.knowledge
+                ) {
+                  return <div className="w-[40px] h-[40px]" />;
+                }
+              },
+              contentRender: (props: ChatItemProps, defaultDom: ReactNode) => {
+                const originData = props.originData || {};
+                if (originData?.role === Role.user) {
+                  return defaultDom;
+                }
+                const message = originData.content;
+                const defaultMessageContent = (
+                  <div
+                    className="leftMessageContent"
+                    style={{ minWidth: messageMinWidth }}
+                  >
+                    {defaultDom}
+                  </div>
+                );
 
-              if (!message || !message.includes('<TOOL>')) {
-                return defaultMessageContent;
-              }
-
-              const [toolStr, answerStr] = message.split('<ANSWER>');
-              const tools = toolStr.split('\n').filter(Boolean);
-              const lastTool = tools[tools.length - 1];
-
-              const regex = /<TOOL>(.*)/;
-              const match = lastTool.match(regex);
-
-              if (!match) {
-                console.error('No valid JSON found in input');
-                return defaultMessageContent;
-              }
-
-              try {
-                const config = JSON.parse(match[1]);
-                const { type, extra } = config;
-
-                if (![Role.knowledge, Role.tool].includes(type)) {
+                if (!message || !message.includes('<TOOL>')) {
                   return defaultMessageContent;
                 }
 
-                const { status, source } = extra;
+                const [toolStr, answerStr] = message.split('<ANSWER>');
+                const tools = toolStr.split('\n').filter(Boolean);
+                const lastTool = tools[tools.length - 1];
 
-                return (
-                  <div
-                    className="p-2 bg-white rounded-md "
-                    style={{ minWidth: messageMinWidth }}
-                  >
-                    <div className="mb-1">
-                      <ThoughtChain
-                        content={extra}
-                        status={status}
-                        source={source}
-                      />
+                const regex = /<TOOL>(.*)/;
+                const match = lastTool.match(regex);
+
+                if (!match) {
+                  console.error('No valid JSON found in input');
+                  return defaultMessageContent;
+                }
+
+                try {
+                  const config = JSON.parse(match[1]);
+                  const { type, extra } = config;
+
+                  if (![Role.knowledge, Role.tool].includes(type)) {
+                    return defaultMessageContent;
+                  }
+
+                  const { status, source } = extra;
+
+                  return (
+                    <div
+                      className="p-2 bg-white rounded-md "
+                      style={{ minWidth: messageMinWidth }}
+                    >
+                      <div className="mb-1">
+                        <ThoughtChain
+                          content={extra}
+                          status={status}
+                          source={source}
+                        />
+                      </div>
+                      <Markdown
+                        style={{ overflowX: 'hidden', overflowY: 'auto' }}
+                      >
+                        {answerStr}
+                      </Markdown>
                     </div>
-                    <Markdown style={{ overflowX: 'hidden', overflowY: 'auto' }}>
-                      {answerStr}
-                    </Markdown>
-                  </div>
-                );
-              } catch (error) {
-                console.error(`JSON parse error: ${error}`);
-                return defaultMessageContent;
-              }
-            },
-          }}
-          assistantMeta={{
-            avatar: assistantMeta?.avatar || BOT_INFO.avatar ,
-            title: assistantMeta?.title || BOT_INFO.resourceName,
-            backgroundColor: assistantMeta?.backgroundColor
-          }}
-          autocompleteRequest={async (value) => {
-            if (value === '/') {
-             const questions = suggested_questions ?? BOT_INFO.work_info.suggested_questions
-              return questions.map(
-                (question: string) => ({
+                  );
+                } catch (error) {
+                  console.error(`JSON parse error: ${error}`);
+                  return defaultMessageContent;
+                }
+              },
+            }}
+            assistantMeta={{
+              avatar: assistantMeta?.avatar || BOT_INFO.avatar,
+              title: assistantMeta?.title || BOT_INFO.resourceName,
+              backgroundColor: assistantMeta?.backgroundColor,
+            }}
+            autocompleteRequest={async (value) => {
+              if (value === '/') {
+                const questions = starters ?? BOT_INFO.work_info.starters;
+                return questions.map((question: string) => ({
                   value: question,
                   label: question,
-                }),
+                }));
+              }
+              return [];
+            }}
+            request={async (messages) => {
+              const newMessages = messages
+                .filter(
+                  (item) =>
+                    item.role !== Role.tool && item.role !== Role.knowledge,
+                )
+                .map((message) => ({
+                  role: message.role,
+                  content: message.content as string,
+                }));
+
+              const response = await streamChat(newMessages, apiUrl, prompt, token);
+              return handleStream(response);
+            }}
+            inputAreaRender={(
+              _: ReactNode,
+              onMessageSend: (message: string) => void | Promise<any>,
+              onClear: () => void,
+            ) => {
+              return (
+                <InputArea
+                  isShowStop={!!proChatRef?.current?.getChatLoadingId()}
+                  onMessageSend={onMessageSend}
+                  onClear={onClear}
+                  onStop={() => proChatRef?.current?.stopGenerateMessage()}
+                />
               );
-            }
-            return [];
-          }}
-          request={async (messages) => {
-            const newMessages = messages
-              .filter(
-                (item) => item.role !== Role.tool && item.role !== Role.knowledge,
-              )
-              .map((message) => ({
-                role: message.role,
-                content: message.content as string,
-              }));
-
-            const response = await streamChat(newMessages, apiUrl);
-            return handleStream(response);
-          }}
-          inputAreaRender={(    
-            _: ReactNode,
-            onMessageSend: (message: string) => void | Promise<any>,
-            onClear: () => void,
-          ) => {
-            return <InputArea isShowStop={!!proChatRef?.current?.getChatLoadingId()} onMessageSend={onMessageSend} onClear={onClear} onStop={() => proChatRef?.current?.stopGenerateMessage()}/>;
-
-          }}
-
-          inputAreaProps={{ className: 'userInputBox h-24 !important' }}
-          actions={{
-            render: () => [
-              <StopBtn
-                key="StopBtn"
-                visible={!!proChatRef?.current?.getChatLoadingId()}
-                action={() => proChatRef?.current?.stopGenerateMessage()}
-              />,
-              <Actions key="Actions"></Actions>,
-            ],
-            flexConfig: {
-              gap: 24,
-              direction: 'vertical',
-              justify: 'space-between',
-            },
-          }}
-      />
-    </div>
-    </div>
-   
-  );
-});
+            }}
+            inputAreaProps={{ className: 'userInputBox h-24 !important' }}
+            actions={{
+              render: () => [
+                <StopBtn
+                  key="StopBtn"
+                  visible={!!proChatRef?.current?.getChatLoadingId()}
+                  action={() => proChatRef?.current?.stopGenerateMessage()}
+                />,
+                <Actions key="Actions"></Actions>,
+              ],
+              flexConfig: {
+                gap: 24,
+                direction: 'vertical',
+                justify: 'space-between',
+              },
+            }}
+          />
+        </div>
+      </div>
+    );
+  },
+);
 
 export default Chat;
--- server/agent/qa_chat.py
+++ server/agent/qa_chat.py
@@ -2,6 +2,7 @@ from typing import AsyncIterator
 from data_class import ChatData
 from agent.base import AgentBuilder
 from prompts.bot_template import generate_prompt_by_repo_name
+from db.supabase.client import get_client
 from tools import issue, sourcecode, knowledge
 
 
@@ -13,12 +14,23 @@ TOOL_MAPPING = {
     "search_code": sourcecode.search_code,
 }
 
+def init_prompt(input_data: ChatData):
+    if input_data.prompt:
+       prompt = input_data.prompt
+    elif input_data.bot_id:
+        supabase = get_client()
+        res=supabase.table("bots").select('prompt').eq('id', input_data.bot_id).execute()
+        print('data', res.data[0]['prompt'])
+        prompt = res.data[0]['prompt']
+    else:
+       prompt = generate_prompt_by_repo_name("ant-design")
+   
+    return prompt
+
 def agent_stream_chat(input_data: ChatData) -> AsyncIterator[str]:
-   prompt = generate_prompt_by_repo_name("ant-design")
-   agent = AgentBuilder(prompt=prompt, tools=TOOL_MAPPING)
-   return agent.run_stream_chat(input_data)
+    agent = AgentBuilder(prompt=init_prompt(input_data), tools=TOOL_MAPPING)
+    return agent.run_stream_chat(input_data)
 
 def agent_chat(input_data: ChatData) -> AsyncIterator[str]:
-    prompt = generate_prompt_by_repo_name("ant-design")
-    agent = AgentBuilder(prompt=prompt, tools=TOOL_MAPPING)
+    agent = AgentBuilder(prompt=init_prompt(input_data), tools=TOOL_MAPPING)
     return agent.run_chat(input_data)
