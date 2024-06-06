e792cbc1a16e5ffeb2293e951d29a05bbac2c833
yumou.zhangym <yumou.zhangym@antgroup.com>
2024-06-06 18:29:11 +0800

    feat: add white domain

--- server/main.py
+++ server/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://petercat.chat"],
+    allow_origins=["http://localhost:3000","http://127.0.0.1:3000","https://petercat.chat"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
