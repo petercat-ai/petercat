0a453955bf9b7b8221cbafbf04844b7914f28118
yingying <yingying.xwy@antgroup.com>
2024-06-25 17:45:33 +0800

    feat: add loading lottie

--- client/components/FullPageSkeleton.tsx
+++ client/components/FullPageSkeleton.tsx
@@ -1,9 +1,14 @@
-import { Spinner } from '@nextui-org/react';
+import Lottie from 'lottie-react';
+import peterCatWalkAnimation from '../public/loading.json';
 
 const FullPageSkeleton = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center bg-white bg-opacity-75">
-      <Spinner />
+      <Lottie
+        animationData={peterCatWalkAnimation}
+        loop={true}
+        style={{ width: '50%', height: '50%' }}
+      />
     </div>
   );
 };
