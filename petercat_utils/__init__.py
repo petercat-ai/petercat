d7f45a35be254169a77862611ee31e1e5837e6b3
MadratJerry <MadratJerry@outlook.com>
2024-08-18 16:17:53 +0800

    fix: petercat export

--- petercat_utils/__init__.py
+++ petercat_utils/__init__.py
@@ -1,11 +1,15 @@
 from .db.client.supabase import get_client
+from .rag_helper import github_file_loader, retrieval, issue_retrieval, task, git_task, git_issue_task, git_doc_task
 from .utils.env import get_env_variable
-from .rag_helper import github_file_loader, retrieval, task
 
 __all__ = [
-  "get_client",
-  "get_env_variable",
-  "github_file_loader",
-  "retrieval",
-  "task"
+    "get_client",
+    "get_env_variable",
+    "github_file_loader",
+    "retrieval",
+    "issue_retrieval",
+    "task",
+    "git_task",
+    "git_issue_task",
+    "git_doc_task"
 ]
