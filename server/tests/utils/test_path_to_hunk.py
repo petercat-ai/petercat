78b36fdf02250f41dc95fc2304b20624485229b5
raoha.rh <raohai.rh@antgroup.com>
2024-08-26 16:15:48 +0800

    feat: update prompts

--- server/tests/utils/test_path_to_hunk.py
+++ server/tests/utils/test_path_to_hunk.py
@@ -14,8 +14,24 @@ basic_patch = '''
     }
   }
 '''
+basic_hunk = '''---new_hunk---
+3:   function greet(name) {
+4:     if (name) {
+5:     return `Hello, ${name}`;
+6:     } else {
+7:       return 'Hello, world!';
+8:     }
+9:   }
+---old_hunk---
+  function greet(name) {
+    if (name) {
+    return 'Hello, ' + name;
+    } else {
+      return 'Hello, world!';
+    }
+  }'''
 
-patch = '''
+long_patch = '''
 @@ -6,7 +6,7 @@
  from langchain.agents.format_scratchpad.openai_tools import (
      format_to_openai_tool_messages,
@@ -49,13 +65,54 @@ patch = '''
      async def run_stream_chat(self, input_data: ChatData) -> AsyncIterator[str]:
 '''
 
+long_hunk = '''---new_hunk---
+6:  from langchain.agents.format_scratchpad.openai_tools import (
+7:      format_to_openai_tool_messages,
+8:  )
+9: from langchain_core.messages import AIMessage, FunctionMessage, HumanMessage, SystemMessage
+10:  from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+11:  from langchain.prompts import MessagesPlaceholder
+12:  from langchain_core.prompts import ChatPromptTemplate
+92:      def chat_history_transform(self, messages: list[Message]):
+93:          transformed_messages = []
+94:          for message in messages:
+95:             match message.role:
+96:                 case "user":
+97:                     transformed_messages.append(HumanMessage(self.chat_model.parse_content(content=message.content)))
+98:                 case "assistant":
+99:                     transformed_messages.append(AIMessage(content=message.content))
+100:                 case "system":
+101:                     transformed_messages.append(SystemMessage(content=message.content))
+102:                 case _:
+103:                     transformed_messages.append(FunctionMessage(content=message.content))
+104:          return transformed_messages
+105:  
+106:      async def run_stream_chat(self, input_data: ChatData) -> AsyncIterator[str]:
+---old_hunk---
+ from langchain.agents.format_scratchpad.openai_tools import (
+     format_to_openai_tool_messages,
+ )
+from langchain_core.messages import AIMessage, FunctionMessage, HumanMessage
+ from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
+ from langchain.prompts import MessagesPlaceholder
+ from langchain_core.prompts import ChatPromptTemplate
+     def chat_history_transform(self, messages: list[Message]):
+         transformed_messages = []
+         for message in messages:
+            if message.role == "user":
+                transformed_messages.append(HumanMessage(self.chat_model.parse_content(content=message.content)))
+            elif message.role == "assistant":
+                transformed_messages.append(AIMessage(content=message.content))
+            else:
+                transformed_messages.append(FunctionMessage(content=message.content))
+         return transformed_messages
+ 
+     async def run_stream_chat(self, input_data: ChatData) -> AsyncIterator[str]:'''
 class TestPathToHunk(TestCase):
   def test_basic_covnert(self):
     result = convert_patch_to_hunk(basic_patch)
-    print(f"result={result}")
-    self.assertTrue(True)
+    self.assertEqual(result.replace("\\n", "\n"), basic_hunk)
 
   def test_parse(self):
-    result = convert_patch_to_hunk(patch)
-    print(f"result={result}")
-    self.assertTrue(True)
+    result = convert_patch_to_hunk(long_patch)
+    self.assertEqual(result, long_hunk.replace("\\n", "\n"))
