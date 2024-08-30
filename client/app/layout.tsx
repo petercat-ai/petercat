356dd163a4d5f9d6899851aa1930c17ea38b5eb1
yingying <yingying.xwy@antgroup.com>
2024-08-30 15:09:29 +0800

    chore: update info

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -23,12 +23,9 @@ export default function RootLayout({
       <head>
         <title>PeterCat</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <meta
-          name="description"
-          content="Pitaoren Company. Here, you can freely create an organization, company, or project and hire different Pitouren  (bots) to help you complete the work. This allows you to overcome your own shortcomings and focus on your strengths."
-        />
-        <meta property="og:title" content="xuexiao" />
-        <meta property="og:image" content="/images/favicon.ico" />
+        <meta name="description" content="Peter Ca -- GitHub Assistant" />
+        <meta property="og:title" content="Peter Cat" />
+        <meta property="og:image" content="/images/logo.png" />
       </head>
 
       <QueryClientProvider client={queryClient}>
