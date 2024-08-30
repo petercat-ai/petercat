5e5757a1194fe90137f00a2ec3f12a5e64275ff1
raoha.rh <raohai.rh@antgroup.com>
2024-08-30 14:12:30 +0800

    feat: using petercat.ai

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -7,7 +7,7 @@ def test_health_checker():
     response = client.get("/api/health_checker")
     assert response.status_code == 200
     assert response.json() == {
-        'API_URL': 'http://127.0.0.1:3000',
+        'API_URL': 'http://127.0.0.1:8000',
         'CALLBACK_URL': 'http://127.0.0.1:8000/api/auth/callback',
-        'WEB_URL': 'http://127.0.0.1:8000',
+        'WEB_URL': 'http://127.0.0.1:3000',
     }
