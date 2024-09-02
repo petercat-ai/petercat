dc302416ebc96c3717b821c33eed846b09a49010
raoha.rh <raohai.rh@antgroup.com>
2024-09-02 23:35:32 +0800

    chore: cleanup envs

--- server/aws/dependencies.py
+++ server/aws/dependencies.py
@@ -1,8 +1,8 @@
-from .constants import AWS_REGION_NAME
+from .constants import AWS_REGION
 import boto3
 
 
 def get_s3_client():
     session = boto3.session.Session()
-    client = session.client(service_name="s3", region_name=AWS_REGION_NAME)
+    client = session.client(service_name="s3", region_name=AWS_REGION)
     return client
