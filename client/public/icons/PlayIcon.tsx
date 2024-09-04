18421237affaba03e7a7c1ea12d65ea0cac8af2f
yingying <yingying.xwy@antgroup.com>
2024-09-04 23:55:13 +0800

    chore: update the icon

--- client/public/icons/PlayIcon.tsx
+++ client/public/icons/PlayIcon.tsx
@@ -8,9 +8,9 @@ const PlayIcon = () => (
     focusable="false"
   >
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-      clip-rule="evenodd"
+      clipRule="evenodd"
     ></path>
   </svg>
 );
