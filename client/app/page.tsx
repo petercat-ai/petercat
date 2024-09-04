0063dc3c1b557a1f1712099adffda719845dfe23
PeachScript <scdzwyxst@gmail.com>
2024-09-04 11:03:20 +0800

    refactor: imporve homepage responsibility

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,12 @@
 'use client';
-import { RefObject, useCallback, useEffect, useRef, useState } from 'react';
+import {
+  Ref,
+  RefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import Image from 'next/image';
 import Fullpage, { fullpageOptions } from '@fullpage/react-fullpage';
 import Lottie, { LottieRefCurrentProps } from 'lottie-react';
@@ -13,22 +20,27 @@ const PC_EXAMPLE_VIDEO = [
   'https://mass-office.alipay.com/huamei_koqzbu/afts/file/LOrpQ66m2xoAAAAAAAAAABAADnV5AQBr',
   'https://mass-office.alipay.com/huamei_koqzbu/afts/file/iKvmQ7J0hLoAAAAAAAAAABAADnV5AQBr',
   'https://mass-office.alipay.com/huamei_koqzbu/afts/file/LIxDSqvF240AAAAAAAAAABAADnV5AQBr',
-][Math.floor(Math.random() * 4)];
+];
 const MOBILE_EXAMPLE_VIDEO = [
   'https://mass-office.alipay.com/huamei_koqzbu/afts/file/OIwnTaSfkrgAAAAAAAAAABAADnV5AQBr',
   'https://mass-office.alipay.com/huamei_koqzbu/afts/file/rzD7RbDSlOIAAAAAAAAAABAADnV5AQBr',
   'https://mass-office.alipay.com/huamei_koqzbu/afts/file/h78QS4sjtP8AAAAAAAAAABAADnV5AQBr',
   'https://mass-office.alipay.com/huamei_koqzbu/afts/file/uevzRKomLYUAAAAAAAAAABAADnV5AQBr',
-][Math.floor(Math.random() * 5)];
+];
 
 export default function Homepage() {
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRefs = {
+    banner: useRef<HTMLVideoElement>(null),
+    pcCase: useRef<HTMLVideoElement & { _timer: number }>(null),
+    mobileCase: useRef<HTMLVideoElement & { _timer: number }>(null),
+  };
   const bannerActionRef = useRef<HTMLDivElement>(null);
   const lightningCatRef = useRef<LottieRefCurrentProps>(null);
   const helixCatRef = useRef<LottieRefCurrentProps>(null);
   const helixOctopusRef = useRef<LottieRefCurrentProps>(null);
   const tableRef = useRef<HTMLDivElement>(null);
   const showCaseRef = useRef<HTMLDivElement>(null);
+  const [videos, setVideos] = useState<{ pc: string; mobile: string }>();
 
   const scrollHandler = useCallback<
     NonNullable<fullpageOptions['onScrollOverflow']>
@@ -83,7 +95,7 @@ export default function Homepage() {
     (_, dest) => {
       if (dest.isFirst) {
         requestAnimationFrame(() => {
-          videoRef.current?.play();
+          videoRefs.banner.current?.play();
           updateClasses(false, false);
         });
       } else {
@@ -113,6 +125,15 @@ export default function Homepage() {
         lightningCatRef.current!.goToAndPlay(0);
       } else if (dest.index === 3) {
         updateClasses(false, true);
+
+        // play video after transition
+        clearTimeout(videoRefs.pcCase.current!._timer);
+        videoRefs.pcCase.current!._timer = window.setTimeout(() => {
+          videoRefs.pcCase.current!.currentTime = 0;
+          videoRefs.pcCase.current!.play();
+          videoRefs.mobileCase.current!.currentTime = 0;
+          videoRefs.mobileCase.current!.play();
+        }, 1333);
       }
     },
     [],
@@ -120,18 +141,41 @@ export default function Homepage() {
   const [stars, setStars] = useState(0);
 
   useEffect(() => {
-    videoRef.current?.addEventListener('timeupdate', () => {
-      if (videoRef.current!.currentTime > videoRef.current!.duration - 0.02) {
-        videoRef.current!.currentTime = 5;
-        videoRef.current!.play();
+    const videoUpdateHandler = () => {
+      if (
+        videoRefs.banner.current!.currentTime >
+        videoRefs.banner.current!.duration - 0.02
+      ) {
+        videoRefs.banner.current!.currentTime = 5;
+        videoRefs.banner.current!.play();
+      } else if (videoRefs.banner.current!.currentTime > 2.5) {
+        // try to display banner action for large screen (no scroll bar)
+        // @ts-ignore
+        scrollHandler();
       }
-    });
+    };
+
+    videoRefs.banner.current?.addEventListener(
+      'timeupdate',
+      videoUpdateHandler,
+    );
 
     fetch('https://api.github.com/repos/petercat-ai/petercat')
       .then((res) => res.json())
       .then((data) => {
         setStars(data.stargazers_count || 0);
       });
+
+    setVideos({
+      pc: PC_EXAMPLE_VIDEO[Math.floor(Math.random() * 3)],
+      mobile: MOBILE_EXAMPLE_VIDEO[Math.floor(Math.random() * 4)],
+    });
+
+    return () =>
+      videoRefs.banner.current?.removeEventListener(
+        'timeupdate',
+        videoUpdateHandler,
+      );
   }, []);
 
   return (
@@ -142,8 +186,8 @@ export default function Homepage() {
       credits={{}}
       render={() => (
         <Fullpage.Wrapper>
-          <div className="section bg-black">
-            <header className="h-20 max-w-[1400px] mx-auto flex items-center">
+          <div className="section min-w-[900px] bg-black">
+            <header className="h-20 px-10 mx-auto flex items-center">
               <span className="flex-1">
                 <Image
                   width={114}
@@ -186,13 +230,13 @@ export default function Homepage() {
                 </a>
               </div>
             </header>
-            <div className="relative">
+            <div className="relative min-h-[calc(100vh-80px)]">
               <video
                 className="mx-auto"
                 width={1400}
                 height={1200}
                 src="https://gw.alipayobjects.com/v/huamei_ghirdt/afts/video/A*CeZ5TJsdJfMAAAAAAAAAAAAADuH-AQ"
-                ref={videoRef}
+                ref={videoRefs.banner}
                 autoPlay
                 muted
               />
@@ -223,9 +267,9 @@ export default function Homepage() {
               </div>
             </div>
           </div>
-          <div className="section bg-black group">
+          <div className="section min-w-[900px] bg-black group">
             <div className="h-screen border-box border-[20px] border-solid border-black bg-[#FEF4E1] rounded-[48px]">
-              <div className="relative h-full flex flex-col justify-center max-w-[1600px] mx-auto py-8 p-16 pt-[110px] opacity-0 transition-opacity group-[.active]:opacity-100">
+              <div className="relative h-full flex flex-col justify-center max-w-[1600px] mx-auto py-8 p-16 pt-10 xl:pt-[110px] opacity-0 transition-opacity group-[.active]:opacity-100">
                 <Image
                   width={475}
                   height={95}
@@ -233,7 +277,7 @@ export default function Homepage() {
                   src="/images/title_features.svg"
                   alt="Features"
                 />
-                <p className="ml-6 text-xl text-[#27272A] mr-[748px] opacity-0 transition-opacity group-[.fp-completely]:opacity-100">
+                <p className="w-1/2 ml-6 text-xl text-[#27272A] opacity-0 transition-opacity group-[.fp-completely]:opacity-100">
                   我们提供对话式答疑 Agent
                   配置系统、自托管部署方案和便捷的一体化应用
                   SDK，让您能够为自己的 GitHub
@@ -241,53 +285,53 @@ export default function Homepage() {
                   为社区提供更高效的技术支持生态。
                 </p>
                 <div
-                  className="w-full relative mt-[72px] overflow-hidden"
+                  className="w-full relative mt-5 xl:mt-[72px] overflow-hidden"
                   ref={tableRef}
                 >
                   <table className="table-fixed border-collapse ">
                     <tbody>
                       <tr>
-                        <td className="relative px-10 py-[51.5px] w-[calc(100%/3)] translate-y-8 opacity-0 transition-all group-[.fp-completely]:delay-300 group-[.fp-completely]:translate-y-0 group-[.fp-completely]:opacity-100">
+                        <td className="relative px-5 xl:px-10 py-5 xl:py-[51.5px] w-[calc(100%/3)] translate-y-8 opacity-0 transition-all group-[.fp-completely]:delay-300 group-[.fp-completely]:translate-y-0 group-[.fp-completely]:opacity-100">
                           <Image
                             width={72}
                             height={73}
                             src="/images/create.svg"
                             alt="create"
                           />
-                          <h3 className="mt-6 mb-3 font-medium text-4xl text-black leading-[1.4]">
+                          <h3 className="mt-6 mb-3 font-medium text-2xl xl:text-4xl text-black leading-[1.4]">
                             对话即创造
                           </h3>
-                          <p className="text-xl text-zinc-800">
+                          <p className="text-base xl:text-xl text-zinc-800">
                             仅需要告知你的仓库地址或名称，Peter Cat
                             即可自动完成创建机器人的全部流程
                           </p>
                         </td>
-                        <td className="relative px-10 py-[51.5px] w-[calc(100%/3)] translate-y-8 opacity-0 transition-all group-[.fp-completely]:delay-500 group-[.fp-completely]:translate-y-0 group-[.fp-completely]:opacity-100">
+                        <td className="relative px-5 xl:px-10 py-5 xl:py-[51.5px] w-[calc(100%/3)] translate-y-8 opacity-0 transition-all group-[.fp-completely]:delay-500 group-[.fp-completely]:translate-y-0 group-[.fp-completely]:opacity-100">
                           <Image
                             width={72}
                             height={73}
                             src="/images/knowledge.svg"
                             alt="knowledge"
                           />
-                          <h3 className="mt-6 mb-3 font-medium text-4xl text-black leading-[1.4]">
+                          <h3 className="mt-6 mb-3 font-medium text-2xl xl:text-4xl text-black leading-[1.4]">
                             知识自动入库
                           </h3>
-                          <p className="text-xl text-zinc-800">
+                          <p className="text-base xl:text-xl text-zinc-800">
                             机器人创建后，所有相关Github 文档和 issue
                             将自动入库，作为机器人的知识依据
                           </p>
                         </td>
-                        <td className="relative px-10 py-[51.5px] w-[calc(100%/3)] translate-y-8 opacity-0 transition-all group-[.fp-completely]:delay-700 group-[.fp-completely]:translate-y-0 group-[.fp-completely]:opacity-100">
+                        <td className="relative px-5 xl:px-10 py-5 xl:py-[51.5px] w-[calc(100%/3)] translate-y-8 opacity-0 transition-all group-[.fp-completely]:delay-700 group-[.fp-completely]:translate-y-0 group-[.fp-completely]:opacity-100">
                           <Image
                             width={72}
                             height={73}
                             src="/images/integrated.svg"
                             alt="integrated"
                           />
-                          <h3 className="mt-6 mb-3 font-medium text-4xl text-black leading-[1.4]">
+                          <h3 className="mt-6 mb-3 font-medium text-2xl xl:text-4xl text-black leading-[1.4]">
                             多平台集成
                           </h3>
-                          <p className="text-xl text-zinc-800">
+                          <p className="text-base xl:text-xl text-zinc-800">
                             多种集成方式自由选择，如对话应用 SDK
                             集成至官网，Github APP一键安装至 Github 仓库等
                           </p>
@@ -305,7 +349,7 @@ export default function Homepage() {
                   </div>
                 </div>
                 <Lottie
-                  className="absolute bottom-[40%] right-10"
+                  className="absolute w-1/2 top-[20%] right-5 pointer-events-none"
                   animationData={LottieOctopusCat}
                   autoPlay={false}
                   loop={false}
@@ -314,10 +358,10 @@ export default function Homepage() {
               </div>
             </div>
           </div>
-          <div className="section bg-black group relative">
+          <div className="section min-w-[900px] bg-black group relative">
             <div className="mx-auto p-[100px] pb-8 opacity-0 transition-opacity group-[.active]:opacity-100 grid grid-cols-2">
               <Lottie
-                className="absolute bottom-0 -right-[200px] opacity-0 group-[.fp-completely]:opacity-100"
+                className="absolute bottom-0 -right-[200px] pointer-events-none opacity-0 group-[.fp-completely]:opacity-100"
                 animationData={LottieLightningCat}
                 autoPlay={false}
                 loop={false}
@@ -400,10 +444,10 @@ export default function Homepage() {
             </div>
           </div>
           <div
-            className="section bg-[#FEF4E1] group *:relative"
+            className="section min-w-[900px] bg-[#FEF4E1] group *:relative"
             ref={showCaseRef}
           >
-            <div className="absolute z-10 left-0 top-0 w-1/2 h-screen flex justify-center items-end">
+            <div className="absolute z-10 left-0 top-0 w-1/2 h-screen flex justify-center items-end pointer-events-none">
               <Lottie
                 animationData={LottieHelixCat}
                 autoPlay={false}
@@ -436,8 +480,12 @@ export default function Homepage() {
                     </div>
                     <video
                       className="max-h-[60vh] min-h-[383px] opacity-0 transition-opacity group-[.fp-completely]:delay-[1333ms] group-[.fp-completely]:opacity-100"
-                      src={MOBILE_EXAMPLE_VIDEO}
-                    ></video>
+                      src={videos?.mobile}
+                      ref={videoRefs.mobileCase}
+                      autoPlay
+                      loop
+                      muted
+                    />
                   </div>
                 </div>
                 <div className="relative p-2 pt-9">
@@ -456,7 +504,11 @@ export default function Homepage() {
                     </div>
                     <video
                       className="max-h-[55vh] min-h-[400px] opacity-0 transition-opacity group-[.fp-completely]:delay-[1333ms] group-[.fp-completely]:opacity-100"
-                      src={PC_EXAMPLE_VIDEO}
+                      src={videos?.pc}
+                      ref={videoRefs.pcCase}
+                      autoPlay
+                      loop
+                      muted
                     />
                   </div>
                   <span className="circle-border-animation absolute top-2.5 left-4 border rounded-full w-4 h-4" />
@@ -464,45 +516,49 @@ export default function Homepage() {
                   <span className="circle-border-animation absolute top-2.5 left-20 border rounded-full w-4 h-4" />
                 </div>
               </div>
-              <a
-                className="absolute bottom-[8.8%] left-1/2 -translate-x-1/2 py-3 px-8 bg-gray-800 text-xl text-white rounded-full transition-all hover:scale-105 opacity-0 group-[.fp-completely]:delay-[1333ms] group-[.fp-completely]:opacity-100"
-                href="/"
-                target="_blank"
-              >
-                了解更多
-              </a>
+              <span className="absolute z-10 bottom-[8.8%] left-1/2 -translate-x-1/2 opacity-0 transition-opacity group-[.fp-completely]:delay-[1333ms] group-[.fp-completely]:opacity-100">
+                <a
+                  className="inline-block py-3 px-8 bg-gray-800 text-xl text-white rounded-full transition-all hover:scale-105"
+                  href="/"
+                  target="_blank"
+                >
+                  了解更多
+                </a>
+              </span>
             </div>
             <footer className="bg-black">
-              <nav className="flex justify-between items-center max-w-[1400px] mx-auto py-[21px]">
-                <a
-                  className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
-                  href="https://github.com/petercat-ai/petercat"
-                  target="_blank"
-                >
-                  Peter Cat 社区
-                </a>
-                <a
-                  className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
-                  href="https://ant-design.antgroup.com/index-cn"
-                  target="_blank"
-                >
-                  Ant Design
-                </a>
-                <a
-                  className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
-                  href="https://makojs.dev/"
-                  target="_blank"
-                >
-                  Mako
-                </a>
-                <a
-                  className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
-                  href="https://opensource.antgroup.com"
-                  target="_blank"
-                >
-                  蚂蚁开源
-                </a>
-              </nav>
+              <div className="px-10">
+                <nav className="flex justify-between items-center max-w-[1400px] mx-auto py-[21px]">
+                  <a
+                    className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
+                    href="https://github.com/petercat-ai/petercat"
+                    target="_blank"
+                  >
+                    Peter Cat 社区
+                  </a>
+                  <a
+                    className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
+                    href="https://ant-design.antgroup.com/index-cn"
+                    target="_blank"
+                  >
+                    Ant Design
+                  </a>
+                  <a
+                    className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
+                    href="https://makojs.dev/"
+                    target="_blank"
+                  >
+                    Mako
+                  </a>
+                  <a
+                    className="text-base text-[#F4F4F5]/[0.6] transition-colors hover:text-[#F4F4F5]"
+                    href="https://opensource.antgroup.com"
+                    target="_blank"
+                  >
+                    蚂蚁开源
+                  </a>
+                </nav>
+              </div>
               <table className="w-full text-[#FEF4E1]">
                 <tbody>
                   <tr>
@@ -526,7 +582,7 @@ export default function Homepage() {
                       </div>
                     </td>
                     <td
-                      className="border border-r-0 border-[#7F7A71] px-10 py-[51.5px] bg-[url('/images/footer-contribution.png')] bg-contain bg-no-repeat pl-[335px]"
+                      className="border border-r-0 border-[#7F7A71] px-10 py-[51.5px] xl:bg-[url('/images/footer-contribution.png')] bg-contain bg-no-repeat pl-20 xl:pl-[335px]"
                       rowSpan={2}
                     >
                       <img
