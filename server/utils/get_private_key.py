0b25408aa6c1aec0370cff5479211f87bc824969
raoha.rh <raohai.rh@antgroup.com>
2024-09-04 12:02:50 +0800

    feat: upload using cloudfront sign

---
+++ server/utils/get_private_key.py
@@ -0,0 +1,14 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def get_private_key(region_name: str, secret_id: str):
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_id)
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    return get_secret_value_response["SecretString"]
