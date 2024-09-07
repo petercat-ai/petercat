b3c33092161e04f230ec0e8289ad64f0c4b4b780
陆沉 <surgesoft@gmail.com>
2024-09-07 18:30:40 +0800

    feat: add sentry
    接一下 Sentry

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@
 // })
 // module.exports = withBundleAnalyzer({})
 
-module.exports = {
+const nextConfig = {
   ...process.env.NEXT_STANDALONE ? { output: "standalone" } :{},
   webpack: (config, { dev}) => {
     config.resolve.fallback = { http: false, https: false, net: false, tls: false };
@@ -18,3 +18,20 @@ module.exports = {
     return config;
   }
 };
+
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, {
+  org: "petercat",
+  project: "petercat",
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: false, // Can be used to suppress logs
+
+  hideSourceMaps: true,
+  
+  sourcemaps: {
+    disable: true,
+  },
+});
