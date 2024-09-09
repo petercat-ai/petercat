e271978886d0efd105e57d17b099868118e4c93e
raoha.rh <raohai.rh@antgroup.com>
2024-09-09 18:30:03 +0800

    feat: add form

--- client/public/icons/DeleteIcon.tsx
+++ client/public/icons/DeleteIcon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-export const DeleteIcon = (props) => (
+export const DeleteIcon = () => (
   <svg
     aria-hidden="true"
     fill="none"
@@ -8,7 +8,6 @@ export const DeleteIcon = (props) => (
     role="presentation"
     viewBox="0 0 20 20"
     width="1em"
-    {...props}
   >
     <path
       d="M17.5 4.98332C14.725 4.70832 11.9333 4.56665 9.15 4.56665C7.5 4.56665 5.85 4.64998 4.2 4.81665L2.5 4.98332"
