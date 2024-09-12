f6644a6adde42c46b48da29008df4ca4d238b04b
raoha.rh <raohai.rh@antgroup.com>
2024-09-12 10:32:51 +0800

    feat: add migration files

---
+++ migrations/supabase/migrations/20240912023152_remote_schema.sql
@@ -0,0 +1,23 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_user_stats(filter_user_id text, start_date date, end_date date)
+ RETURNS TABLE(usage_date date, input_tokens bigint, output_tokes bigint, total_tokens bigint)
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN QUERY
+    SELECT
+        u.date AS usage_date,  -- 使用别名来避免歧义
+        SUM(u.input_token)::BIGINT AS input_tokens,  -- 将结果转换为 BIGINT
+        SUM(u.output_token)::BIGINT AS output_tokes,  -- 将结果转换为 BIGINT
+        SUM(u.total_token)::BIGINT AS total_tokens  -- 将结果转换为 BIGINT
+    FROM user_token_usage u
+    WHERE u.user_id = filter_user_id  -- 使用别名来引用参数
+        AND u.date >= start_date
+        AND u.date <= end_date
+    GROUP BY u.date;
+END;
+$function$
+;
+
+
