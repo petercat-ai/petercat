b7ef24a3ab1828734445b3727e82e4ac2bb35409
yingying <yingying.xwy@antgroup.com>
2024-09-24 01:43:34 +0800

    fix: enabel to handel the event issue_comment.edited

--- server/event_handler/discussion.py
+++ server/event_handler/discussion.py
@@ -1,10 +1,15 @@
 import requests
-from typing import Any
+from typing import Any, Tuple
 from github import Github, Auth
 from github import GithubException
 from agent.bot.get_bot import get_bot_by_id
 from core.dao.repositoryConfigDAO import RepositoryConfigDAO
 from petercat_utils.data_class import ChatData, Message, TextContentBlock
+from agent.prompts.issue_helper import (
+    generate_issue_comment_prompt,
+    generate_issue_prompt,
+)
+
 
 from agent.qa_chat import agent_chat
 
@@ -89,15 +94,21 @@ class DiscussionEventHandler:
         text_block = TextContentBlock(type="text", text=discussion_content)
         discussion_number = discussion["number"]
         message = Message(role="user", content=[text_block])
-
         repository_config = RepositoryConfigDAO()
         repo_config = repository_config.get_by_repo_name(repo_name)
+
+        prompt = generate_issue_prompt(
+            repo_name=repo_name,
+            issue_url=discussion.html_url,
+            issue_number=discussion.number,
+            issue_content=discussion.body,
+        )
 
         bot = get_bot_by_id(repo_config.robot_id)
 
         analysis_result = await agent_chat(
             ChatData(
-                prompt=bot.prompt,
+                prompt=prompt,
                 messages=[message],
                 bot_id=repo_config.robot_id,
             ),
@@ -121,4 +132,131 @@ class DiscussionEventHandler:
 
         except GithubException as e:
             print(f"处理 GitHub 请求时出错： {e}")
+            return {"success": False, "error": str(e)}
+
+
+class DiscussionCommentEventHandler(DiscussionEventHandler):
+    def not_mentioned_me(self):
+        return "@petercat-bot" not in self.event["comment"]["body"]
+
+    def get_comments(self, owner: str, repo: str, discussion_number: int):
+        access_token = self.auth.token
+        query = """
+        query($owner: String!, $repo: String!, $discussion_number: Int!, $cursor: String) {
+        repository(owner: $owner, name: $repo) {
+            discussion(number: $discussion_number) {
+            comments(first: 100, after: $cursor) {
+                edges {
+                node {
+                    id
+                    body
+                    author {
+                    login
+                    }
+                    createdAt
+                }
+                }
+                pageInfo {
+                endCursor
+                hasNextPage
+                }
+            }
+            }
+        }
+        }
+        """
+
+        headers = {
+            "Authorization": f"Bearer {access_token}",
+            "Accept": "application/vnd.github.v3+json",
+        }
+        variables = {
+            "owner": owner,
+            "repo": repo,
+            "discussion_number": discussion_number,
+            "cursor": None,
+        }
+
+        comments = []
+        has_next_page = True
+        cursor = None
+
+        while has_next_page:
+            variables["cursor"] = cursor
+            json_data = {"query": query, "variables": variables}
+            response = requests.post(self.graph_url, headers=headers, json=json_data)
+            if response.status_code == 200:
+                result = response.json()
+                discussion_comments = result["data"]["repository"]["discussion"][
+                    "comments"
+                ]
+                for edge in discussion_comments["edges"]:
+                    comments.append(edge["node"])
+
+                # 分页信息
+                cursor = discussion_comments["pageInfo"]["endCursor"]
+                has_next_page = discussion_comments["pageInfo"]["hasNextPage"]
+            else:
+                raise Exception(
+                    f"Query failed to run by returning code of {response.status_code}. {response.text}"
+                )
+
+        return comments
+
+    async def execute(self):
+        try:
+            print(f"actions={self.event['action']},sender={self.event['sender']}")
+            # 忽略机器人回复
+            if self.event["sender"]["type"] == "Bot":
+                return {"success": True}
+            if self.event["action"] in ["created", "edited"]:
+                if self.not_mentioned_me():
+                    return {"success": True}
+                discussion = self.event["discussion"]
+                discussion_number = discussion["number"]
+                owner = self.event["organization"]["login"]
+                repo_name = self.event["repository"]["full_name"]
+                discussion_content = f"{discussion['title']}: {discussion['body']}"
+                comments = self.get_comments(
+                    owner, self.event["repository"]["name"], discussion_number
+                )
+                print("comments", comments)
+                messages = [
+                    Message(
+                        role="assistant" if comment.user.type == "Bot" else "user",
+                        content=[TextContentBlock(type="text", text=comment.body)],
+                    )
+                    for comment in comments
+                ]
+                print("messages", messages)
+                repository_config = RepositoryConfigDAO()
+                repo_config = repository_config.get_by_repo_name(repo_name)
+
+                bot = get_bot_by_id(repo_config.robot_id)
+
+                prompt = generate_issue_comment_prompt(
+                    repo_name=repo_name,
+                    issue_url=discussion.html_url,
+                    issue_content=discussion_content,
+                )
+                print("promptprompt", prompt)
+
+                analysis_result = await agent_chat(
+                    ChatData(
+                        prompt=prompt,
+                        messages=messages,
+                        bot_id=repo_config.robot_id,
+                    ),
+                    self.auth,
+                    bot,
+                )
+                discussion_id = await self.get_discussion_id(
+                    owner, self.event["repository"]["name"], discussion_number
+                )
+                await self.create_discussion_comment(
+                    discussion_id, analysis_result["output"]
+                )
+
+        except GithubException as e:
+            print(f"处理 GitHub 请求时出错：{e}")
             return {"success": False, "error": str(e)}
