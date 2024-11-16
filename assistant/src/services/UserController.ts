8f04ad13c4a160cbebda9a8410384a8e2b68ee64
SedationH <sedationh@gmail.com>
2024-11-16 11:32:48 +0800

    fix: remove apiDomain var in assistant package

--- assistant/src/services/UserController.ts
+++ assistant/src/services/UserController.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
-
 //  Get the public bot profile by id
 export async function getUserInfo(apiDomain: string, { clientId }: { clientId?: string }) {  
   const response = await axios.get(`${apiDomain}/api/auth/userinfo?clientId=${clientId}`, { withCredentials: true });
