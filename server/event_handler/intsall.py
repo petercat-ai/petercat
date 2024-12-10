52646b4ea6d329935a04ae47605f4955f1c30ad4
yingying <yingying.xwy@antgroup.com>
2024-12-10 11:54:06 +0800

    feat: add the InstallationEditEventHandler

--- server/event_handler/intsall.py
+++ server/event_handler/intsall.py
@@ -6,7 +6,7 @@ from core.models.repository import RepositoryConfig
 import time
 
 
-class InstallEventHandler:
+class InstallationEventHandler:
     event: Any
     auth: Auth.AppAuth
     g: Github
@@ -45,6 +45,52 @@ class InstallEventHandler:
                 self.delete_config()
                 return {"success": True}
             if action == "created":
+                self.add_config()
+                return {"success": True}
+        except GithubException as e:
+            print(f"处理 GitHub 请求时出错：{e}")
+            return {"success": False, "error": str(e)}
+
+
+class InstallationEditEventHandler:
+    event: Any
+    auth: Auth.AppAuth
+    g: Github
+
+    def __init__(self, payload: Any, auth: Auth.AppAuth, installation_id: int) -> None:
+        self.event: Any = payload
+        self.auth: Auth.AppAuth = auth
+        self.g: Github = Github(auth=auth)
+
+    def delete_config(self):
+        repositories = self.event["repositories_removed"]
+        repo_ids = [str(repo["id"]) for repo in repositories]
+        repository_config = RepositoryConfigDAO()
+        repository_config.delete_by_repo_ids(repo_ids)
+
+    def add_config(self):
+        repositories = self.event["repositories_added"]
+        owner_id = self.event["installation"]["account"]["id"]
+        repository_config_dao = RepositoryConfigDAO()
+        repository_configs: List[RepositoryConfig] = []
+        for repo in repositories:
+            repository_config = RepositoryConfig(
+                owner_id=str(owner_id),
+                repo_name=repo["full_name"],
+                repo_id=str(repo["id"]),
+                robot_id="",
+                created_at=int(time.time()),
+            )
+            repository_configs.append(repository_config)
+        repository_config_dao.create_batch(repository_configs)
+
+    async def execute(self):
+        try:
+            action = self.event["action"]
+            if action == "removed":
+                self.delete_config()
+                return {"success": True}
+            if action == "added":
                 self.add_config()
                 return {"success": True}
         except GithubException as e:
