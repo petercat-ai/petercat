e3f89e18be3587c51dcde62ec6a65d0745da4c86
Peter Dave Hello <hsu@peterdavehello.org>
2025-01-02 02:05:13 +0800

    "Improve" zh-TW locale a bit

--- client/.kiwi/zh-TW/app.ts
+++ client/.kiwi/zh-TW/app.ts
@@ -2,33 +2,33 @@ export default {
   page: {
     woMenLaiZiMa:
       '我們來自螞蟻集團支付寶體驗技術部，致力於創造美而實用的產品，AI\n                          只是手段，為你的工作提供更多愉悅的價值才是我們的唯一目標。',
-    xiangMuXiangXiXin: '項目詳細信息請進文檔查閱',
-    chaKanGengDuo: '查看更多',
+    xiangMuXiangXiXin: '專案詳細資訊請進文件查閱',
+    chaKanGengDuo: '檢視更多',
     maYiKaiYuan: '螞蟻開源',
-    agreement: '用户协议',
-    agreementLabel: '我已閱讀並同意用戶協議',
+    agreement: '使用者協議',
+    agreementLabel: '我已閱讀並同意使用者協議',
     policy: '隱私政策',
-    pETER: 'PeterCat 社區',
+    pETER: 'PeterCat 社群',
     liaoJieGengDuo: '了解更多',
     deYiYuQiangDa:
-      '得益於強大的底層能力，您可以將任意感興趣的代碼倉庫轉換為答疑機器人，或體驗社區中其它機器人。它們不僅能推薦優質代碼倉庫，還能協助用戶自動提交\n                    issue。',
+      '得益於強大的底層能力，您可以將任意感興趣的程式碼倉庫轉換為答疑機器人，或體驗社群中其它機器人。它們不僅能推薦優質程式碼倉庫，還能協助使用者自動提交\n                    issue。',
     duoZhongJiChengFang:
-      '多種集成方式自由選擇，如對話應用 SDK\n                            集成至官網，Github APP一鍵安裝至 Github 倉庫等',
-    duoPingTaiJiCheng: '多平台集成',
+      '多種整合方式自由選擇，如對話應用 SDK\n                            整合至官網，Github APP 一鍵安裝至 Github 倉庫等',
+    duoPingTaiJiCheng: '多平臺整合',
     jiQiRenChuangJian:
-      '機器人創建後，所有相關Github 文檔和 issue\n                            將自動入庫，作為機器人的知識依據',
+      '機器人建立後，所有相關 Github 文件和 issue\n                            將自動入庫，作為機器人的知識依據',
     zhiShiZiDongRu: '知識自動入庫',
     jinXuYaoGaoZhi:
-      '僅需要告知你的倉庫地址或名稱，PeterCat\n                            即可自動完成創建機器人的全部流程',
+      '僅需要告知你的倉庫地址或名稱，PeterCat\n                            即可自動完成建立機器人的全部流程',
     duiHuaJiChuangZao: '對話即創造',
     woMenTiGongDui:
-      '我們提供對話式答疑 Agent\n                  配置系統、自託管部署方案和便捷的一體化應用\n                  SDK，讓您能夠為自己的 GitHub\n                  倉庫一鍵創建智能答疑機器人，並快速集成到各類官網或項目中，\n                  為社區提供更高效的技術支持生態。',
+      '我們提供對話式答疑 Agent\n                  配置系統、自託管部署方案和便捷的一體化應用\n                  SDK，讓您能夠為自己的 GitHub\n                  倉庫一鍵建立智慧答疑機器人，並快速整合到各類官網或專案中，\n                  為社群提供更高效的技術支援生態。',
     liJiChangShi: '立即嘗試',
-    shiZhuanWeiSheQu: '是專為社區維護者和開發者打造的智能答疑機器人解決方案',
+    shiZhuanWeiSheQu: '是專為社群維護者和開發者打造的智慧答疑機器人解決方案',
     xiaoMaoMiZhuNi: '小貓咪助你征服 Github',
-    wenDang: '文檔',
+    wenDang: '文件',
     release: '更新日誌',
-    gongZuoTai: '工作台',
+    gongZuoTai: '工作臺',
     yanShiAnLi: '演示案例',
   },
 };
