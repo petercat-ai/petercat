1f184e2c884479472a00fbc06ad7b734135fb0fa
Peter Dave Hello <hsu@peterdavehello.org>
2025-01-02 21:43:44 +0800

    "Improve" zh-TW locale a bit (#649)

--- client/.kiwi/zh-TW/DeployBotModal.ts
+++ client/.kiwi/zh-TW/DeployBotModal.ts
@@ -1,23 +1,23 @@
 export default {
   DeployContent: {
-    buShuDaoQiTa: '部署到其他平台',
-    shuaXin: '刷新',
+    buShuDaoQiTa: '部署到其他平臺',
+    shuaXin: '重新整理',
     qianWangAnZhuang: '前往安裝',
     weiNengHuoQuNin:
-      '未能獲取您在 GITHUB 平台上已經安裝 petercat assistant 助手的倉庫，請',
+      '未能取得您在 GITHUB 平臺上已經安裝 petercat assistant 助手的倉庫，請',
     zheJiangTiHuanCang: '這將替換倉庫原有機器人',
-    dianJiKaiShiPei: '點擊開始配置',
+    dianJiKaiShiPei: '點選開始配置',
     xuanZeYaoGuanLian: '選擇要關聯的 GitHub 倉庫',
-    qingShuRuYuMing: '請輸入正确網域名稱，以 https:// 開始',
+    qingShuRuYuMing: '請輸入正確網域名稱，以 https:// 開始',
     muBiaoWangZhanYu: '目標網站域名',
     shenHeZhong: '審核中...',
     buShuDaoWoDe: '部署到我的網站',
     zheJiangTiJiaoYi:
-      '這將提交一個 issue 到 PeterCat 倉庫，待我們人工審核通過後即可完成公開。',
-    ninDeJiQiRen: '您的機器人已經公開到了市場，請前往市場查看。',
+      '這將提交一個 issue 到 PeterCat 倉庫，待我們人工審核透過後即可完成公開。',
+    ninDeJiQiRen: '您的機器人已經公開到了市場，請前往市場檢視。',
     gongKaiDaoPE: '公開到 PeterCat 市場',
     meiZhaoDaoXiangYao:"找不到已安裝 PeterCat Assistant 的倉庫",
-    dianJiCiChu:'點擊此處',
+    dianJiCiChu:'點選此處',
     shouQuanAnZhuangG:"授權組織給 PeterCat"
   },
   DeployItem: {
@@ -25,12 +25,12 @@ export default {
   },
   index: {
     tiaoGuo: '跳過',
-    baoCunChengGong: '保存成功！',
+    baoCunChengGong: '儲存成功！',
     buShuChengGong: '部署成功',
     wanCheng: '完成',
     tiJiaoChengGong: '提交成功',
     shouQiBuShu: '收起',
     buShu: '部署',
-    daKaiBuShu: '打开部署',
+    daKaiBuShu: '開啟部署',
   },
 };
--- client/.kiwi/zh-TW/release.ts
+++ client/.kiwi/zh-TW/release.ts
@@ -1,11 +1,11 @@
 export default {
   page: {
-    zuiXinChanPinXin: '最新產品新聞，以便在新版能力發布時獲取最新信息',
+    zuiXinChanPinXin: '最新產品新聞，以便在新版能力發布時取得最新資訊',
     guanZhu: '關注',
-    xiaoMaoMiJiQi: '小貓咪機器人首次亮相上海外灘大會，助力開源社區答疑場景',
+    xiaoMaoMiJiQi: '小貓咪機器人首次亮相上海外灘大會，助力開源社群答疑場景',
     waiTanDaHuiShou: '外灘大會首次發布',
     quanLianLuGuoJi:
-      '全鏈路國際化支持，增加官網移動端適配，多平台集成鏈路產品化',
-    guoJiHuaZhiChi: '國際化支持',
+      '全鏈路國際化支援，增加官網移動端適配，多平臺整合鏈路產品化',
+    guoJiHuaZhiChi: '國際化支援',
   },
 };
