3ede81060a5552d7a82e0f0ca1037fb3fb2b1691
yingying <yingying.xwy@antgroup.com>
2025-01-20 16:13:57 +0800

    feat: add the api for pr&issue&code insight

--- server/insight/router.py
+++ server/insight/router.py
@@ -2,6 +2,7 @@ import json
 from typing import Optional
 from fastapi import APIRouter, Depends
 from insight.service.issue import get_issue_data
+from insight.service.pr import get_code_changes, get_pr_data
 
 
 router = APIRouter(
@@ -15,6 +16,32 @@ router = APIRouter(
 def get_issue_insight(repo_name: str):
     try:
         result = get_issue_data(repo_name)
+        return {
+            "success": True,
+            "data": result,
+        }
+
+    except Exception as e:
+        return json.dumps({"success": False, "message": str(e)})
+
+
+@router.get("/pr")
+def get_pr_insight(repo_name: str):
+    try:
+        result = get_pr_data(repo_name)
+        return {
+            "success": True,
+            "data": result,
+        }
+
+    except Exception as e:
+        return json.dumps({"success": False, "message": str(e)})
+
+
+@router.get("code_change")
+def get_code_change_insight(repo_name: str):
+    try:
+        result = get_code_changes(repo_name)
         return {
             "success": True,
             "data": result,
--- server/insight/service/issue.py
+++ server/insight/service/issue.py
@@ -1,6 +1,8 @@
 import requests
 from collections import defaultdict
 
+from utils.insight import get_data
+
 
 def get_issue_data(repo_name):
     metrics_mapping = {
@@ -8,44 +10,5 @@ def get_issue_data(repo_name):
         "issues_closed": "close",
         "issue_comments": "comment",
     }
-
-    base_url = f"https://oss.open-digger.cn/github/{repo_name}/"
-
-    aggregated_data = {
-        "year": defaultdict(lambda: {"open": 0, "close": 0, "comment": 0}),
-        "quarter": defaultdict(lambda: {"open": 0, "close": 0, "comment": 0}),
-        "month": defaultdict(lambda: {"open": 0, "close": 0, "comment": 0}),
-    }
-
-    for metric, metric_type in metrics_mapping.items():
-        url = f"{base_url}{metric}.json"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            data = response.json()
-            for date, value in data.items():
-                if "-" in date:  # 只处理日期数据
-                    year, month = date.split("-")[:2]
-                    quarter = f"{year}Q{(int(month) - 1) // 3 + 1}"
-
-                    # 按年度、季度、月度分别聚合
-                    aggregated_data["year"][year][metric_type] += value
-                    aggregated_data["quarter"][quarter][metric_type] += value
-                    aggregated_data["month"][date][metric_type] += value
-        else:
-            print(
-                f"Error fetching data from {url} (status code: {response.status_code})"
-            )
-
-    def format_result(data):
-        result = []
-        for date, counts in data.items():
-            for type_, value in counts.items():
-                result.append({"type": type_, "date": date, "value": value})
-        return result
-
-    return {
-        "year": format_result(aggregated_data["year"]),
-        "quarter": format_result(aggregated_data["quarter"]),
-        "month": format_result(aggregated_data["month"]),
-    }
+    issue_data = get_data(repo_name, metrics_mapping)
+    return issue_data
