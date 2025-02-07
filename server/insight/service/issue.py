e8881cd303fa9fff3e4ba49fca8f9e03702dc35c
yingying <yingying.xwy@antgroup.com>
2025-02-07 11:08:48 +0800

    chore: fix ci

--- server/insight/service/issue.py
+++ server/insight/service/issue.py
@@ -17,42 +17,46 @@ def get_issue_resolution_duration(repo_name):
     url = (
         f"https://oss.open-digger.cn/github/{repo_name}/issue_resolution_duration.json"
     )
-    response = requests.get(url)
-    data = response.json()
+    try:
+        response = requests.get(url)
+        data = response.json()
 
-    quantile_keys = [
-        "quantile_0",
-        "quantile_1",
-        "quantile_2",
-        "quantile_3",
-        "quantile_4",
-    ]
-    quantile_data = {k: v for k, v in data.items() if k in quantile_keys}
+        quantile_keys = [
+            "quantile_0",
+            "quantile_1",
+            "quantile_2",
+            "quantile_3",
+            "quantile_4",
+        ]
+        quantile_data = {k: v for k, v in data.items() if k in quantile_keys}
 
-    all_time_keys = set()
-    for qk in quantile_data:
-        all_time_keys.update(quantile_data[qk].keys())
+        all_time_keys = set()
+        for qk in quantile_data:
+            all_time_keys.update(quantile_data[qk].keys())
 
-    year_pattern = re.compile(r"^\d{4}$")
-    quarter_pattern = re.compile(r"^\d{4}Q[1-4]$")
-    month_pattern = re.compile(r"^\d{4}-\d{2}$")
+        year_pattern = re.compile(r"^\d{4}$")
+        quarter_pattern = re.compile(r"^\d{4}Q[1-4]$")
+        month_pattern = re.compile(r"^\d{4}-\d{2}$")
 
-    result = {"year": [], "quarter": [], "month": []}
+        result = {"year": [], "quarter": [], "month": []}
 
-    for key in all_time_keys:
-        values = [quantile_data[qk].get(key, 0) for qk in quantile_keys]
+        for key in all_time_keys:
+            values = [quantile_data[qk].get(key, 0) for qk in quantile_keys]
 
-        if year_pattern.match(key):
-            result["year"].append({"date": key, "value": values})
-        elif quarter_pattern.match(key):
-            result["quarter"].append({"date": key, "value": values})
-        elif month_pattern.match(key):
-            result["month"].append({"date": key, "value": values})
+            if year_pattern.match(key):
+                result["year"].append({"date": key, "value": values})
+            elif quarter_pattern.match(key):
+                result["quarter"].append({"date": key, "value": values})
+            elif month_pattern.match(key):
+                result["month"].append({"date": key, "value": values})
 
-    result["year"].sort(key=lambda x: int(x["date"]))
+        result["year"].sort(key=lambda x: int(x["date"]))
 
-    result["quarter"].sort(key=lambda x: (int(x["date"][:4]), int(x["date"][-1])))
+        result["quarter"].sort(key=lambda x: (int(x["date"][:4]), int(x["date"][-1])))
 
-    result["month"].sort(key=lambda x: tuple(map(int, x["date"].split("-"))))
+        result["month"].sort(key=lambda x: tuple(map(int, x["date"].split("-"))))
 
-    return result
+        return result
+    except Exception as e:
+        print(e)
+        return []
